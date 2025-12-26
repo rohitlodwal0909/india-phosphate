d6f0fd2e90de4ec91e24d80252839fd994fabeb2
manalsoftech <manalsoftechmspl@gmail.com>
2025-12-26 13:21:16 +0530

    change

--- client/src/constants/contant.tsx
+++ client/src/constants/contant.tsx
@@ -2,8 +2,3 @@ export const apiUrl = "https://erp.indiaphosphate.com/api";
 export const fakeUrl = "https://fake-json-api.mock.beeceptor.com/users";
 export const ImageUrl = "https://erp.indiaphosphate.com";
 
-
-// export const apiUrl = "http://127.0.0.1:5000/api";
-// export const fakeUrl = "https://fake-json-api.mock.beeceptor.com/users";
-// export const ImageUrl = "http://127.0.0.1:5000";
-
