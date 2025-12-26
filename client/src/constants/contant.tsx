c1a44b191c2b5380eb2b17854b53aa4b55a1f213
manalsoftech <manalsoftechmspl@gmail.com>
2025-12-26 13:15:05 +0530

    Constant chnage

--- client/src/constants/contant.tsx
+++ client/src/constants/contant.tsx
@@ -1,6 +1,6 @@
-export const apiUrl = "http://72.61.235.133:5000/api";
+export const apiUrl = "https://erp.indiaphosphate.com/api";
 export const fakeUrl = "https://fake-json-api.mock.beeceptor.com/users";
-export const ImageUrl = "http://72.61.235.133:5000";
+export const ImageUrl = "https://erp.indiaphosphate.com";
 
 
 // export const apiUrl = "http://127.0.0.1:5000/api";
