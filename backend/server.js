6d0f894f489da25abebadf71d7854e94c3375dd1
manalsoftech <manalsoftechmspl@gmail.com>
2026-06-25 19:59:14 +0530

    company ip set

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 });
 
-const allowedIPs = ["1.187.173.17", "127.0.0.1"];
+const allowedIPs = ["172.18.0.1", "127.0.0.1"];
 
 app.use((req, res, next) => {
   let clientIP =
