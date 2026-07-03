3992f38a3a1d75fb7f3cc911623f1ed05ba05911
manalsoftech <manalsoftechmspl@gmail.com>
2026-07-03 11:50:06 +0530

    d

--- client/src/constants/contant.tsx
+++ client/src/constants/contant.tsx
@@ -5,3 +5,6 @@ export const ImageUrl = 'https://erp.indiaphosphate.com/';
 // export const apiUrl = 'http://127.0.0.1:5000/api';
 // export const fakeUrl = 'https://fake-json-api.mock.beeceptor.com/users';
 // export const ImageUrl = 'http://127.0.0.1:5000/';
+// export const ImageUrl = 'http://127.0.0.1:5000/';
+
+// jjjj
--- client/src/views/dashboard/WelcomeDashboard.tsx.tsx
+++ client/src/views/dashboard/WelcomeDashboard.tsx.tsx
@@ -16,7 +16,8 @@ const WelcomeDashboard: React.FC = () => {
   const id = logindata?.admin?.id || null;
 
   const dispatch = useDispatch<AppDispatch>();
-  //
+
+  //afeqv
 
   const totalcustomer = useSelector((state: RootState) => state.customerdashboard.totalcustomers);
 
